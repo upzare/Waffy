7d8ed961da10352ed96fa341230ea28b8d568924
Aravind Manoj <aravindmng47@gmail.com>
2025-05-02 10:09:15 +0530

    fix: codeblock style update

--- src/sidepanel/utils/RenderResponse.jsx
+++ src/sidepanel/utils/RenderResponse.jsx
@@ -1,69 +1,76 @@
-import { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
+import { useState } from "react";
+import Markdown from "react-markdown";
+import { Copy, Check } from "lucide-react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { funky } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function RenderResponse({ content }) {
-    const Pre = ({ children }) => {
-        const [copyIcon, setCopyIcon] = useState((<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z" /></svg>));
-
-        const handleClick = () => {
-            navigator.clipboard.writeText(children.props.children);
-            setCopyIcon((<svg style={{ fill: "#75dc6d" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" /></svg>));
-            setTimeout(() => {
-                setCopyIcon((<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z" /></svg>));
-            }, 2000);
-        }
-
-        const match = /language-(\w+)/.exec(children.props.className || "");
-        const language = match ? match[1].toUpperCase() : "TEXT";
-        return (
-            <pre className="code-block">
-                <div className="code-header">
-                    <span className="code-language">{language}</span>
-                    <div
-                        className="code-copy-btn"
-                        title="Copy Code"
-                        onClick={handleClick}
-                    >
-                        {copyIcon}
-                    </div>
-                </div>
-                <div className="code-content">{children}</div>
-            </pre>
-        );
+  const Pre = ({ children }) => {
+    const [CopyIcon, setCopyIcon] = useState(Copy);
+    
+    const handleClick = () => {
+      navigator.clipboard.writeText(children.props.children);
+      setCopyIcon(Check);
+      setTimeout(() => {
+        setCopyIcon(Copy);
+      }, 2000);
     }
-
+    
+    const match = /language-(\w+)/.exec(children.props.className || "");
+    const language = match ? match[1].toUpperCase() : "TEXT";
+    
     return (
-        <ReactMarkdown
-            className="markdown-content"
-            linkTarget="_blank"
-            rehypePlugins={[rehypeRaw]}
-            remarkPlugins={[remarkGfm]}
-            components={{
-                pre: Pre,
-                code({ node, inline, className = "code", children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || "")
-                    return !inline && match ? (
-                        <SyntaxHighlighter
-                            style={a11yDark}
-                            language={match[1]}
-                            PreTag="div"
-                            {...props}
-                        >
-                            {String(children).replace(/\n$/, "")}
-                        </SyntaxHighlighter>
-                    ) : (
-                        <code className={className} {...props}>
-                            {children}
-                        </code>
-                    )
-                }
-            }}
-        >
-            {content}
-        </ReactMarkdown>
-    )
-}
+      <pre className="code-block">
+        <div className="code-header">
+          <span className="language-label">{language}</span>
+          <div
+            className="copy-button"
+            title="Copy Code"
+            onClick={handleClick}
+          >
+            {<CopyIcon />}
+          </div>
+        </div>
+        <div className="code-content">{children}</div>
+      </pre>
+    );
+  }
+  
+  return (
+    <Markdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        pre: Pre,
+        code({ node, inline, className = "code", children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "")
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={funky}
+              language={match[1]}
+              PreTag="div"
+              className="syntax-highlighter"
+              {...props}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        },
+        div(props) {
+          return (
+            <div className="overflow-container">
+              {props.children}
+            </div>
+          );
+        }
+      }}
+      className="markdown-content"
+    >
+      {content}
+    </Markdown>
+  );
+};
