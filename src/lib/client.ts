5548eda5ef69b8869b9d7e1711947fc3d06212a1
Aravind Manoj <aravindmng47@gmail.com>
2025-06-22 19:16:07 +0530

    fix: localstorage struct

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -26,10 +26,9 @@ export const initSettings = async () => {
                 enableNotifications: true,
             }),
             data: JSON.stringify({
-                waffyAPI: "1234",
                 signed: true,
                 account: {
-                    account_id: "c9c20661-a918-4ef5-b805-d7273ccb79f6",
+                    account_id: "",
                     email: "",
                     name: "",
                     phone_number: "",
